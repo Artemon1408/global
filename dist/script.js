6c7d5e56ff6406c71773c2525d68a5c09ffaf328
Artem <12artemondrozhzhyn@gmail.com>
2023-01-04 13:46:47 +0200

    change country

--- dist/script.js
+++ dist/script.js
@@ -206,56 +206,56 @@ const modal = () => {
     const trigger = document.querySelectorAll(triggerSelector),
           modal = document.querySelector(modalSelector),
           close = document.querySelector(closeSelector),
-          windows = document.querySelectorAll('[data-modal]'),
+          windows = document.querySelectorAll("[data-modal]"),
           scroll = calcScroll();
     trigger.forEach(item => {
-      item.addEventListener('click', e => {
+      item.addEventListener("click", e => {
         if (e.target) {
           e.preventDefault();
         }
 
         windows.forEach(item => {
-          item.style.display = 'none';
+          item.style.display = "none";
         });
-        modal.style.display = 'block';
-        document.body.style.overflow = 'hidden';
+        modal.style.display = "block";
+        document.body.style.overflow = "hidden";
         document.body.style.marginRight = `${scroll}px`;
       });
     });
-    close.addEventListener('click', () => {
+    close.addEventListener("click", () => {
       windows.forEach(item => {
-        item.style.display = 'none';
+        item.style.display = "none";
       });
-      modal.style.display = 'none';
-      document.body.style.overflow = '';
+      modal.style.display = "none";
+      document.body.style.overflow = "";
       document.body.style.marginRight = `0px`;
     });
-    modal.addEventListener('click', e => {
+    modal.addEventListener("click", e => {
       if (e.target === modal) {
-        windows.forEach(item => {
-          item.style.display = 'none';
-        });
-        modal.style.display = 'none';
-        document.body.style.overflow = '';
+        // windows.forEach((item) => {
+        //   item.style.display = "none";
+        // });
+        modal.style.display = "none";
+        document.body.style.overflow = "";
         document.body.style.marginRight = `0px`;
       }
     });
   }
 
   function calcScroll() {
-    let div = document.createElement('div');
-    div.style.width = '50px';
-    div.style.height = '50px';
-    div.style.overflowY = 'scroll';
-    div.style.visibility = 'hidden';
+    let div = document.createElement("div");
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.overflowY = "scroll";
+    div.style.visibility = "hidden";
     document.body.appendChild(div);
     let scrollWidth = div.offsetWidth - div.clientWidth;
     div.remove();
     return scrollWidth;
   }
 
-  bindModal('.btn__call', '.modal', '.modal_close');
-  bindModal('.promo__btn', '.modal', '.modal_close');
+  bindModal(".btn__call", ".modal", ".modal_close");
+  bindModal(".promo__btn", ".modal", ".modal_close");
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (modal);
